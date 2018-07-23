0fb5aa290713aebe39e3b351040b66c86068e8a6
Prashant Vishwakarma <32397516+prashant-vishwakarma@users.noreply.github.com>
2018-07-23 20:46:41 +0530

    Update Main.java

--- K-Means/src/Main.java
+++ K-Means/src/Main.java
@@ -50,30 +50,29 @@ public class Main {
 		clusterCounts = new int[k];
 
 		Random random = new Random();
-		
-		//random centroids
+
+		// random centroids
 		for (i = 0; i < k; i++) {
 			centroids[i][0] = random.nextInt((int) ((maxX - minX) + 1)) + minX;
 			centroids[i][1] = random.nextInt((int) ((maxY - minY) + 1)) + minY;
-			//System.out.println("Centroid#" + (i + 1) + ": " + centroids[i][0] + "," + centroids[i][1]);
+			// System.out.println("Centroid#" + (i + 1) + ": " + centroids[i][0] + "," +
+			// centroids[i][1]);
 		}
-		
+
 		printCentroids(centroids, k);
-		
-		//calculate distances
-		/*for (i = 0; i < k; i++) {
-			for (int j = 0; j < count; j++) {
-				distances[i][j] = argument(centroids[i][0], centroids[i][1], points[j][0], points[j][1]);
-			}
-		}*/
+
+		// calculate distances
+		/*
+		 * for (i = 0; i < k; i++) { for (int j = 0; j < count; j++) { distances[i][j] =
+		 * argument(centroids[i][0], centroids[i][1], points[j][0], points[j][1]); } }
+		 */
 		distances = calculateDistances(centroids, points, k, count);
 
 		int minI = 0;
-		
-		//findMinimum I
+
+		// findMinimum I
 		for (int j = 0; j < count; j++) {
 			for (i = 0; i < k; i++) {
-				// minDistance = i == 0 ? distances[i][j] : minDistance;
 				if (i == 0) {
 					minDistance = distances[i][j];
 					minI = 0;
@@ -83,10 +82,65 @@ public class Main {
 					minI = i;
 				}
 			}
-			clusters[minI][clusterCounts[minI]++] = j;//build cluster
+			clusters[minI][clusterCounts[minI]++] = j;// build cluster
 		}
 
-		for (i = 0; i < k; i++) {
+		centroids = calculateCentroids(clusters, clusterCounts, k, centroids);
+
+		printCentroids(centroids, k);
+
+		System.out.println("End Reached");
+	}// end of main
+	
+	static int[][] buildClusters(double[][] distances,int count, int k){
+		double minDistance = 1000;
+		int[][] clusters = new int[k][count];
+		int[] clusterCounts = new int[k];
+		for (int j = 0; j < count; j++) {
+			int minI = 0;
+			for (int i = 0; i < k; i++) {
+				if (i == 0) {
+					minDistance = distances[i][j];
+					minI  = 0;
+				}
+				if (minDistance > distances[i][j]) {
+					minDistance = distances[i][j];
+					minI = i;
+				}
+			}
+			clusters[minI][clusterCounts[minI]++] = j;// build cluster
+		}
+		return clusters;
+	}
+
+	static double argument(double x1, double y1, double x2, double y2) {
+		return Math.sqrt(Math.pow((y2 - y1), 2) + Math.pow((x2 - x1), 2));
+	}
+
+	static void printCentroids(double centroids[][], int k) {
+		System.out.println("");
+		for (int i = 0; i < k; i++) {
+			System.out.println("Centroid#" + (i + 1) + ": " + centroids[i][0] + "," + centroids[i][1]);
+		}
+	}
+
+	static double[][] calculateDistances(double centroids[][], double points[][], int k, int count) {
+		double[][] distances = new double[k][count];
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < count; j++) {
+				distances[i][j] = argument(centroids[i][0], centroids[i][1], points[j][0], points[j][1]);
+			}
+		}
+		return distances;
+	}
+
+	static double[][] calculateCentroids(int[][] clusters, int[] clusterCounts, int k, double[][] prevCentroids) {
+		double[][] centroids = new double[k][2];
+		for (int i = 0; i < k; i++) {
+			centroids[i][0] = prevCentroids[i][0];
+			centroids[i][1] = prevCentroids[i][1];
+		}
+		for (int i = 0; i < k; i++) {
 			if (clusterCounts[i] != 0) {
 				int sumX = 0;
 				int sumY = 0;
@@ -100,30 +154,8 @@ public class Main {
 				centroids[i][1] = sumY;
 			}
 		}
-
-		printCentroids(centroids, k);
-
-		System.out.println("End Reached");
-	}//end of main
-
-	static double argument(double x1, double y1, double x2, double y2) {
-		return Math.sqrt(Math.pow((y2 - y1), 2) + Math.pow((x2 - x1), 2));
-	}
-
-	static void printCentroids(double centroids[][], int k) {
-		System.out.println("");
-		for (int i = 0; i < k; i++) {
-			System.out.println("Centroid#" + (i + 1) + ": " + centroids[i][0] + "," + centroids[i][1]);
-		}
+		return centroids;
 	}
 	
-	static double[][] calculateDistances(double centroids[][],double points[][], int k, int count){
-		double[][] distances = new double[k][count];
-		for (int i = 0; i < k; i++) {
-			for (int j = 0; j < count; j++) {
-				distances[i][j] = argument(centroids[i][0], centroids[i][1], points[j][0], points[j][1]);
-			}
-		}
-		return distances;
-	}
+
 }
